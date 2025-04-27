82eb845c81202296e45ec4faa7a35b21c8844861
Raigester <vovaraigester@gmail.com>
2025-04-27 19:25:40 +0300

    fix: Update phone number validation messages to Ukrainian for localization

--- utils/data_validation.py
+++ utils/data_validation.py
@@ -6,6 +6,6 @@ def validate_phone_number(value):
     try:
         parsed_number = phonenumbers.parse(value, None)
         if not phonenumbers.is_valid_number(parsed_number):
-            raise ValidationError(f"{value} is not a valid phone number.")
+            raise ValidationError(f"{value} не є дійсним номером телефону.")
     except phonenumbers.NumberParseException:
-        raise ValidationError(f"{value} is not a valid phone number format.")
+        raise ValidationError(f"{value} не є дійсним форматом номера телефону.")
